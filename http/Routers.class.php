ec816b3214a48e1bdeae2802bac8cc4bc31497fb
manjutapali <tmanjunath04@gmail.com>
2017-06-20 14:34:52 +0530

    Route files

---
+++ http/Routers.class.php
@@ -0,0 +1,22 @@
+<?php
+
+	//include '/controllers/user.php';
+
+	class Router
+	{
+		public static function get($controller, $function, $param)
+		{
+
+		}
+
+		public static function post($controller, $function, $param)
+		{
+
+		}
+
+		public static function update($controller, $function, $param)
+		{
+
+		}
+	}
+?>
