2e780cc77ac16301449ee6f70d21f732844f7b19
manjutapali <tmanjunath04@gmail.com>
2017-06-21 12:06:14 +0530

    Resolved routing homepage(/) bug

--- http/Routers.class.php
+++ http/Routers.class.php
@@ -1,12 +1,17 @@
 <?php
 
-	//include '/controllers/user.php';
-
+	include 'controllers/user.php';
+	
 	class Router
 	{
-		public static function get($controller, $function, $param)
+		public static function get($controller, $function="", $param="")
 		{
 
+			//echo "Loading the ". $controller ." controller, calling " .$function. "<br>";
+
+			$obj = new $controller;
+
+			$obj->{$function}();
 		}
 
 		public static function post($controller, $function, $param)
--- http/Routes.php
+++ http/Routes.php
@@ -1,7 +1,25 @@
 <?php
 	
-	echo $_SERVER['REQUEST_URI'];
+	include 'Routers.class.php';
 
 	$url = $_SERVER['REQUEST_URI'];
-	
+
+	// Spliting the url to get controller, function and parameter
+	$url_split = explode("/", $url);
+
+	// 3rd index holds the controller name
+
+	if(isset($url_split[3]))
+	{
+		switch ($url_split[3]) 
+		{
+			case 'user':
+				isset($url_split[4]) ? Router::get('user', $url_split[4]) : Router::get('user');
+				break;
+			
+			default:
+				
+				break;
+		}
+	}
 ?>
